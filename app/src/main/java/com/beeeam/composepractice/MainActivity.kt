d55dc2d2ba2192f8b9a8c5c0f4edbfda03c68193
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-11-26 22:54:23 +0900

    3. Slot API 연습

--- app/src/main/java/com/beeeam/composepractice/MainActivity.kt
+++ app/src/main/java/com/beeeam/composepractice/MainActivity.kt
@@ -1,24 +1,34 @@
 package com.beeeam.composepractice
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.beeeam.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,9 +39,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Composable1()
+                    MainScreen()
                 }
             }
         }
@@ -39,130 +49,133 @@ class MainActivity : ComponentActivity() {
 }
 
 // @Composable
-// fun DemoScreen() {
-//    var textState by rememberSaveable { mutableStateOf("") }
-//
-//    val onTextChange = { text: String ->
-//        textState = text
+// fun SlotDemo(
+//    topContent: @Composable () -> Unit,
+//    midContent: @Composable () -> Unit,
+//    bottomContent: @Composable () -> Unit,
+// ) {
+//    Column {
+//        topContent()
+//        midContent()
+//        bottomContent()
 //    }
-//
-//    MyTextField(textState = textState, onTextChange = onTextChange)
 // }
 //
-// @OptIn(ExperimentalMaterial3Api::class)
 // @Composable
-// fun MyTextField(textState: String, onTextChange: (String) -> Unit) {
-//    TextField(
-//        value = textState,
-//        onValueChange = { onTextChange(it) }
-//    )
+// fun SlotButton() {
+//    Button(onClick = { }) {
+//        Text("Click Me!!")
+//    }
 // }
-
+//
 // @Preview
 // @Composable
 // fun Preview() {
 //    ComposePracticeTheme {
-//        DemoScreen()
+//        SlotDemo(
+//            topContent = { Text(text = "Top Content") },
+//            midContent = { SlotButton() },
+//            bottomContent = { Text(text = "Bottom Content") }
+//        )
 //    }
 // }
-
-// @Composable
-// fun FunctionA() {
-//    var switchState by remember { mutableStateOf(true) }
-//
-//    val onSwitchChange = { value: Boolean ->
-//        switchState = value
-//    }
-//
-//    FunctionB(
-//        switchState = switchState,
-//        onSwitchChange = { onSwitchChange(it) }
-//    )
-// }
-//
-// @Composable
-// fun FunctionB(switchState: Boolean, onSwitchChange: (Boolean) -> Unit) {
-//    Switch(
-//        checked = switchState,
-//        onCheckedChange = onSwitchChange
-//    )
-// }
-//
-// @Preview
-// @Composable
-// fun PreView() {
-//    ComposePracticeTheme {
-//        Column {
-//            FunctionA()
-//        }
-//    }
-// }
-
-val LocalColor = staticCompositionLocalOf { Color(0xFFffdbcf) }
 
 @Composable
-fun Composable1() {
-    var color = if (isSystemInDarkTheme()) {
-        Color(0xFFa08d87)
-    } else {
-        Color(0xFFffdbcf)
-    }
-    Column {
-        Composable2()
+fun TitleImage(drawing: Int) {
+    Image(painter = painterResource(drawing), contentDescription = "Title Image")
+}
 
-        CompositionLocalProvider(LocalColor provides color) {
-            Composable3()
+@Composable
+fun MainScreen() {
+    var linearSelected by remember { mutableStateOf(true) }
+    var imageSelected by remember { mutableStateOf(true) }
+
+    val onLinearClick = { value: Boolean ->
+        linearSelected = value
+    }
+
+    val onImageClick = { value: Boolean ->
+        imageSelected = value
+    }
+
+    ScreenContent(
+        linearSelected = linearSelected,
+        imageSelected = imageSelected,
+        onLinearClick = onLinearClick,
+        onImageClick = onImageClick,
+        titleContent = {
+            if (imageSelected) {
+                TitleImage(drawing = R.drawable.baseline_cloud_download_24)
+            } else {
+                Text(text = "DownLoading", modifier = Modifier.padding(30.dp))
+            }
+        },
+        progressContent = {
+            if (linearSelected) {
+                LinearProgressIndicator(Modifier.height(40.dp))
+            } else {
+                CircularProgressIndicator(Modifier.height(200.dp), strokeWidth = 18.dp)
+            }
         }
+    )
+}
+
+@Composable
+fun ScreenContent(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onLinearClick: (Boolean) -> Unit,
+    onImageClick: (Boolean) -> Unit,
+    titleContent: @Composable () -> Unit,
+    progressContent: @Composable () -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        titleContent()
+        progressContent()
+        CheckBoxes(
+            linearSelected = linearSelected,
+            imageSelected = imageSelected,
+            onLinearClick = onLinearClick,
+            onImageClick = onImageClick
+        )
     }
 }
 
 @Composable
-fun Composable2() {
-    Composable4()
-}
+fun CheckBoxes(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onLinearClick: (Boolean) -> Unit,
+    onImageClick: (Boolean) -> Unit,
+) {
+    Row(
+        Modifier.padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(checked = imageSelected, onCheckedChange = onImageClick)
+        Text("Image Title")
 
-@Composable
-fun Composable3() {
-    Composable5()
-}
+        Spacer(modifier = Modifier.width(20.dp))
 
-@Composable
-fun Composable4() {
-    Composable6()
-}
-
-@Composable
-fun Composable5() {
-    Composable7()
-    Composable8()
-}
-
-@Composable
-fun Composable6() {
-    Text("Composable6")
-}
-
-@Composable
-fun Composable7() {
-}
-
-@Composable
-fun Composable8() {
-    Text("Composable8", modifier = Modifier.background(LocalColor.current))
+        Checkbox(checked = linearSelected, onCheckedChange = onLinearClick)
+        Text("Linear Title")
+    }
 }
 
 @Preview
 @Composable
-fun DefaultPreview() {
+fun Preview() {
     ComposePracticeTheme {
-        Composable1()
-    }
-}
-
-@Preview(uiMode = UI_MODE_NIGHT_YES)
-@Composable
-fun DarkPreview() {
-    ComposePracticeTheme {
-        Composable1()
+//        CheckBoxes(
+//            linearSelected = true,
+//            imageSelected = true,
+//            onLinearClick = {  },
+//            onImageClick = {  }
+//        )
+        MainScreen()
     }
 }
