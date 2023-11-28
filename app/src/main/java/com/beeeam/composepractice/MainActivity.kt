d14be7dddc77a7fa0b6fa9992b27af7552bf4bb1
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-11-28 17:35:30 +0900

    5. Row, Column 연습

--- app/src/main/java/com/beeeam/composepractice/MainActivity.kt
+++ app/src/main/java/com/beeeam/composepractice/MainActivity.kt
@@ -3,26 +3,24 @@ package com.beeeam.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,42 +43,145 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DemoScreen() {
-    val modifier = Modifier.border(width = 2.dp, color = Color.Black).padding(10.dp)
-    Column(
-        Modifier.padding(20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Hello Compose",
-            modifier = modifier,
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold
-        )
+fun TextCell(text: String, modifier: Modifier = Modifier) {
+    val cellModifier = Modifier
+        .padding(4.dp)
+        .size(100.dp, 100.dp)
+        .border(width = 4.dp, color = Color.Black)
 
-        Spacer(Modifier.height(16.dp))
-
-        CustomImage(
-            drawing = R.drawable.vacation,
-            modifier = Modifier.padding(10.dp).width(270.dp).clip(shape = RoundedCornerShape(30.dp))
-        )
-    }
+    Text(
+        text = text,
+        cellModifier.then(modifier),
+        fontSize = 70.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
 }
 
 @Composable
-fun CustomImage(drawing: Int, modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(drawing),
-        modifier = modifier,
-        contentDescription = null
-    )
+fun MainScreen() {
+//    Row {
+//        TextCell(text = "1")
+//        TextCell(text = "2")
+//        TextCell(text = "3")
+//    }
+//    Column {
+//        TextCell(text = "1")
+//        TextCell(text = "2")
+//        TextCell(text = "3")
+//    }
+
+//    Column {
+//        Row {
+//            TextCell(text = "1")
+//            TextCell(text = "4")
+//            TextCell(text = "7")
+//        }
+//        Row {
+//            TextCell(text = "2")
+//            TextCell(text = "5")
+//            TextCell(text = "8")
+//        }
+//        Row {
+//            TextCell(text = "3")
+//            TextCell(text = "6")
+//        }
+//        Row {
+//            TextCell(text = "9")
+//            TextCell(text = "10")
+//            TextCell(text = "11")
+//        }
+//    }
+
+//    Row(
+//        verticalAlignment = Alignment.CenterVertically,
+//        modifier = Modifier.size(width = 400.dp, height = 200.dp)
+//    ) {
+//        TextCell(text = "1")
+//        TextCell(text = "2")
+//        TextCell(text = "3")
+//    }
+
+//    Column(
+//        horizontalAlignment = Alignment.End,
+//        modifier = Modifier.size(width = 200.dp, height = 400.dp)
+//    ) {
+//        TextCell(text = "1")
+//        TextCell(text = "2")
+//        TextCell(text = "3")
+//    }
+
+//    Row(
+//        horizontalArrangement = Arrangement.End,
+//        modifier = Modifier.size(width = 400.dp, height = 200.dp)
+//    ) {
+//        TextCell("1")
+//        TextCell("2")
+//        TextCell("3")
+//    }
+
+//    Column(
+//        modifier = Modifier.height(400.dp),
+//        verticalArrangement = Arrangement.Bottom
+//    ) {
+//        TextCell("1")
+//        TextCell("2")
+//        TextCell("3")
+//    }
+
+    Column(
+        modifier = Modifier.size(width = 1000.dp, height = 1000.dp),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Row(
+            modifier = Modifier.width(1000.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            TextCell("1")
+            TextCell("2")
+            TextCell("3")
+        }
+        Row(
+            modifier = Modifier.width(1000.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            TextCell("4")
+            TextCell("5")
+            TextCell("6")
+        }
+        Row(
+            modifier = Modifier.width(1000.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            TextCell("7")
+            TextCell("8")
+            TextCell("9")
+        }
+    }
 }
+
+// @Composable
+// fun TestLabelBtn() {
+//    val modifier = Modifier
+//        .size(width = 41.dp, height = 26.dp)
+//        .padding(start = 6.dp, end = 6.dp, top = 6.dp, bottom = 6.dp)
+//
+//    Button(
+//        onClick = { },
+//    ) {
+//        Text(
+//            fontSize = 12.sp,
+//            text = "label",
+//
+//        )
+//    }
+// }
 
 @Preview
 @Composable
 fun Preview() {
     ComposePracticeTheme {
-        DemoScreen()
+        MainScreen()
+//        TestLabelBtn()
     }
 }
