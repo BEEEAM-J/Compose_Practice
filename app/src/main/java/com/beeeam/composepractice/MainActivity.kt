19b1d0bc26b18d55cc6e064f9f71065e7dda81eb
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-11-28 18:23:03 +0900

    6. Box layout 연습

--- app/src/main/java/com/beeeam/composepractice/MainActivity.kt
+++ app/src/main/java/com/beeeam/composepractice/MainActivity.kt
@@ -5,19 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -47,7 +49,7 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
     val cellModifier = Modifier
         .padding(4.dp)
         .size(100.dp, 100.dp)
-        .border(width = 4.dp, color = Color.Black)
+        .border(width = 5.dp, color = Color.Black)
 
     Text(
         text = text,
@@ -60,128 +62,61 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun MainScreen() {
-//    Row {
-//        TextCell(text = "1")
-//        TextCell(text = "2")
-//        TextCell(text = "3")
-//    }
-//    Column {
-//        TextCell(text = "1")
-//        TextCell(text = "2")
-//        TextCell(text = "3")
+//    Box {
+//        val width = 200.dp
+//        val height = 200.dp
+//
+//        TextCell("1", Modifier.size(width, height))
+//        TextCell("2", Modifier.size(width, height))
+//        TextCell("3", Modifier.size(width, height))
 //    }
 
-//    Column {
-//        Row {
-//            TextCell(text = "1")
-//            TextCell(text = "4")
-//            TextCell(text = "7")
-//        }
-//        Row {
-//            TextCell(text = "2")
-//            TextCell(text = "5")
-//            TextCell(text = "8")
-//        }
-//        Row {
-//            TextCell(text = "3")
-//            TextCell(text = "6")
-//        }
-//        Row {
-//            TextCell(text = "9")
-//            TextCell(text = "10")
-//            TextCell(text = "11")
-//        }
-//    }
-
-//    Row(
-//        verticalAlignment = Alignment.CenterVertically,
-//        modifier = Modifier.size(width = 400.dp, height = 200.dp)
+//    Box(
+//        contentAlignment = Alignment.CenterEnd,
+//        modifier = Modifier.size(400.dp, 400.dp)
 //    ) {
-//        TextCell(text = "1")
-//        TextCell(text = "2")
-//        TextCell(text = "3")
+//        TextCell("1", Modifier.size(width = 200.dp, height = 200.dp))
 //    }
 
-//    Column(
-//        horizontalAlignment = Alignment.End,
-//        modifier = Modifier.size(width = 200.dp, height = 400.dp)
+//    Box(
+//        modifier = Modifier.size(width = 500.dp, height = 500.dp)
 //    ) {
-//        TextCell(text = "1")
-//        TextCell(text = "2")
-//        TextCell(text = "3")
+//        Text("Top Start", Modifier.align(Alignment.TopStart))
+//        Text("Top Center", Modifier.align(Alignment.TopCenter))
+//        Text("Top Bottom", Modifier.align(Alignment.TopEnd))
+//        Text("Center Start", Modifier.align(Alignment.CenterStart))
+//        Text("Center ", Modifier.align(Alignment.Center))
+//        Text("Center End", Modifier.align(Alignment.CenterEnd))
+//        Text("Bottom Start", Modifier.align(Alignment.BottomStart))
+//        Text("Bottom Center", Modifier.align(Alignment.BottomCenter))
+//        Text("Bottom End", Modifier.align(Alignment.BottomEnd))
 //    }
 
-//    Row(
-//        horizontalArrangement = Arrangement.End,
-//        modifier = Modifier.size(width = 400.dp, height = 200.dp)
-//    ) {
-//        TextCell("1")
-//        TextCell("2")
-//        TextCell("3")
-//    }
+//    Box(
+//        modifier = Modifier
+//            .size(200.dp)
+//            .clip(CircleShape)
+//            .background(Color.Blue)
+//    )
+//    Box(
+//        modifier = Modifier
+//            .size(200.dp)
+//            .clip(RoundedCornerShape(30.dp))
+//            .background(Color.Blue)
+//    )
 
-//    Column(
-//        modifier = Modifier.height(400.dp),
-//        verticalArrangement = Arrangement.Bottom
-//    ) {
-//        TextCell("1")
-//        TextCell("2")
-//        TextCell("3")
-//    }
-
-    Column(
-        modifier = Modifier.size(width = 1000.dp, height = 1000.dp),
-        verticalArrangement = Arrangement.SpaceEvenly
-    ) {
-        Row(
-            modifier = Modifier.width(1000.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            TextCell("1")
-            TextCell("2")
-            TextCell("3")
-        }
-        Row(
-            modifier = Modifier.width(1000.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            TextCell("4")
-            TextCell("5")
-            TextCell("6")
-        }
-        Row(
-            modifier = Modifier.width(1000.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            TextCell("7")
-            TextCell("8")
-            TextCell("9")
-        }
-    }
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .clip(CutCornerShape(30.dp))
+            .background(Color.Blue)
+    )
 }
-
-// @Composable
-// fun TestLabelBtn() {
-//    val modifier = Modifier
-//        .size(width = 41.dp, height = 26.dp)
-//        .padding(start = 6.dp, end = 6.dp, top = 6.dp, bottom = 6.dp)
-//
-//    Button(
-//        onClick = { },
-//    ) {
-//        Text(
-//            fontSize = 12.sp,
-//            text = "label",
-//
-//        )
-//    }
-// }
 
 @Preview
 @Composable
 fun Preview() {
     ComposePracticeTheme {
         MainScreen()
-//        TestLabelBtn()
     }
 }
